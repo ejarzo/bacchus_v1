7e3828861da1493429d302f7fa0ca1a8c43b0e95
ejarzo <ejarz25@gmail.com>
2018-03-25 16:07:04 -0400

    add linting

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import React, { Component } from 'react';
 import './App.css';
-import MainMenu from 'components/MainMenu';
+// import MainMenu from 'components/MainMenu';
 import NewsScraper from 'components/NewsScraper';
 
 class App extends Component {
-  render() {
+  render () {
     return (
       <div className="App">
         <NewsScraper />
