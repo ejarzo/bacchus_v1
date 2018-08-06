4a0055c1498d1ff8b0fd9223b7b517ac9dde4d7c
ejarzo <ejarz25@gmail.com>
2018-08-06 08:50:41 -0400

    set up deployments

--- src/index.js
+++ src/index.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { BrowserRouter } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom';
 
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render((
-  <BrowserRouter>
+  <BrowserRouter basename={process.env.PUBLIC_URL} >
     <App />
   </BrowserRouter>
 ), document.getElementById('root'));
